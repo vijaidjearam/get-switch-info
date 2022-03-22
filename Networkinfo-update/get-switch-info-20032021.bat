5bf0fe18f1c17dfbb52aa30c2d96fa25ad4905fa
vijai <vijai.djearam@gmail.com>
2022-03-22 08:07:37 +0100

    Add files via upload

---
+++ Networkinfo-update/get-switch-info-20032021.bat
@@ -0,0 +1,2 @@
+pwsh -nop -c "Get-Content %~dp0get-switch-info-20032021.ps1 | pwsh -noprofile -"
+pause
---
+++ Networkinfo-update/get-switch-info-20032021.ps1
@@ -0,0 +1,8 @@
+﻿[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+Import-Module PSDiscoveryProtocol
+$msg   = 'Enter the Filter for PC::'
+$filter = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
+
+$output = Get-ADComputer -SearchBase "OU=computers,OU=985,OU=URCA,DC=ad-urca,DC=univ-reims,DC=fr" -Filter 'Name -like $filter'|Select-Object -ExpandProperty Name | Out-GridView -PassThru
+Write-Host $output
+$output | ForEach-Object -Parallel {Invoke-DiscoveryProtocolCapture -ComputerName $_ | Get-DiscoveryProtocolData} |Select-Object Computer,Device,Port,PortDescription | Out-GridView
