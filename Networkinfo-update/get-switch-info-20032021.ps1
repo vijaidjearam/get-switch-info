514895faa571658a63472fd20cd1f8416af53a67
vijai <vijai.djearam@gmail.com>
2022-03-23 10:16:21 +0100

    Update get-switch-info-20032021.ps1

--- Networkinfo-update/get-switch-info-20032021.ps1
+++ Networkinfo-update/get-switch-info-20032021.ps1
@@ -10,4 +10,4 @@ $filter = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
 
 $output = Get-ADComputer -SearchBase "OU=computers,OU=985,OU=URCA,DC=ad-urca,DC=univ-reims,DC=fr" -Filter 'Name -like $filter'|Select-Object -ExpandProperty Name | Out-GridView -PassThru
 Write-Host $output
-$output | ForEach-Object -Parallel {Invoke-DiscoveryProtocolCapture -ComputerName $_ | Get-DiscoveryProtocolData} |Select-Object @{Name = "Hostname";  Expression = {extractstring $_.Computer}},@{Name = "Switch";  Expression = {extractstring $_.Device}},Port,PortDescription | Out-GridView
+$output | ForEach-Object -Parallel {Invoke-DiscoveryProtocolCapture -ComputerName $_ | Get-DiscoveryProtocolData} |Select-Object @{Name = "Hostname";  Expression = {extractstring $_.Computer}},@{Name = "Switch";  Expression = {extractstring $_.Device}},Port,PortDescription,Interface | Out-GridView
