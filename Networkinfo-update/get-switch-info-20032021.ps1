ff02ba03207cd83e8460110ebb140419e9772d70
vijai <vijai.djearam@gmail.com>
2022-03-23 08:47:38 +0100

    Added a function to extract hostname
    Added a function to extract hostname from FQDN

--- Networkinfo-update/get-switch-info-20032021.ps1
+++ Networkinfo-update/get-switch-info-20032021.ps1
@@ -1,8 +1,13 @@
-﻿[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
 Import-Module PSDiscoveryProtocol
+function extractstring($temp){
+$temp = $temp.Split(".")
+$temp = $temp[0]
+return $temp
+}
 $msg   = 'Enter the Filter for PC::'
 $filter = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
 
 $output = Get-ADComputer -SearchBase "OU=computers,OU=985,OU=URCA,DC=ad-urca,DC=univ-reims,DC=fr" -Filter 'Name -like $filter'|Select-Object -ExpandProperty Name | Out-GridView -PassThru
 Write-Host $output
-$output | ForEach-Object -Parallel {Invoke-DiscoveryProtocolCapture -ComputerName $_ | Get-DiscoveryProtocolData} |Select-Object Computer,Device,Port,PortDescription | Out-GridView
+$output | ForEach-Object -Parallel {Invoke-DiscoveryProtocolCapture -ComputerName $_ | Get-DiscoveryProtocolData} |Select-Object @{Name = "Hostname";  Expression = {extractstring $_.Computer}},@{Name = "Switch";  Expression = {extractstring $_.Device}},Port,PortDescription | Out-GridView
